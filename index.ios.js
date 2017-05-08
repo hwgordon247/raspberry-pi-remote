0c9a96f1de18ae0901ed9fd097db468120d89d92
hwgordon247 <hwgordon247@gmail.com>
2017-05-08 17:34:35 +0100

    styling of the home page

--- index.ios.js
+++ index.ios.js
@@ -11,15 +11,26 @@ import {
 var MOCKED_MOVIES_DATA = [
     {title: 'Title', year: '2015', posters: {thumbnail: 'https://facebook.github.io/react/img/logo_og.png'}},
 ];
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
 
 export default class RaspberryPiRemote extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            movies:null,
+        };
+    }
+
     render() {
         var movie = MOCKED_MOVIES_DATA[0];
         return (
         <View style={styles.container}>
-            <Text>{movie.title}</Text>
-            <Text>{movie.year}</Text>
             <Image style={styles.thumbnail} source={{uri: 'https://facebook.github.io/react/img/logo_og.png'}} />
+            <View style={styles.rightContainer}>
+                <Text style={styles.title}>{movie.title}</Text>
+                <Text style={styles.year}>{movie.year}</Text>
+            </View>
         </View>
         );
     }
@@ -34,6 +45,24 @@ const styles = StyleSheet.create({
     }, thumbnail: {
         width: 50,
         height: 50,
+    },
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    rightContainer: {
+        flex: 1,
+    },
+    title: {
+        fontSize: 20,
+        marginBottom: 8,
+        textAlign: 'center',
+    },
+    year: {
+        textAlign: 'center',
     },
 });
 
