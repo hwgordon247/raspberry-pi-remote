4de48eb073fa9e087d0746bc3a14177dfbbbe375
Gordon, Henry <Henry.Gordon@capco.com>
2017-05-08 16:31:52 +0100

    hello world starter

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,39 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    StyleSheet,
+    Image,
+    Text,
+    View
 } from 'react-native';
 
+var MOCKED_MOVIES_DATA = [
+    {title: 'Title', year: '2015', posters: {thumbnail: 'https://facebook.github.io/react/img/logo_og.png'}},
+];
+
 export default class RaspberryPiRemote extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+    render() {
+        var movie = MOCKED_MOVIES_DATA[0];
+        return (
+        <View style={styles.container}>
+            <Text>{movie.title}</Text>
+            <Text>{movie.year}</Text>
+            <Image style={styles.thumbnail} source={{uri: 'https://facebook.github.io/react/img/logo_og.png'}} />
+        </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    }, thumbnail: {
+        width: 50,
+        height: 50,
+    },
 });
 
 AppRegistry.registerComponent('RaspberryPiRemote', () => RaspberryPiRemote);
