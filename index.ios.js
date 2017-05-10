5c1c0b457c9d770ed4414081665776a258d4bfe9
hwgordon247 <hwgordon247@gmail.com>
2017-05-10 17:30:18 +0100

    first communication with raspberry pi server

--- index.ios.js
+++ index.ios.js
@@ -10,100 +10,45 @@ import {
     View,
 } from 'react-native';
 
-var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+var REQUEST_URL = 'http://localhost:3000/test';
 
 class RaspberryPiRemote extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            dataSource: new ListView.DataSource({
-                rowHasChanged: (row1, row2) => row1 !== row2,
-            }),
-            loaded: false,
+            isActive: 'loading',
         };
     }
 
     componentDidMount() {
-        this.fetchData();
+        this.fetchPiStatus();
     }
 
-    fetchData() {
+    fetchPiStatus() {
+        console.log('WHREER DOES THIS LOG???')
         fetch(REQUEST_URL)
-        .then((response) => response.json())
+        // .then((response) => response.json())
         .then((responseData) => {
+            console.log(responseData);
             this.setState({
-                dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
-                loaded: true,
+                isActive: responseData._bodyText,
             });
         }).done();
     }
 
     render() {
-        if (!this.state.loaded) {
-            return this.renderLoadingView();
-        }
-
         return (
-            <ListView
-            dataSource={this.state.dataSource}
-            renderRow={this.renderMovie}
-            style={styles.listView}
-            />
-        );
-    }
-
-    renderLoadingView() {
-        return (
-            <View style={styles.container}>
-                <Text>
-                    Loading movies...
-                </Text>
-            </View>
-        );
-    }
-
-    renderMovie(movie) {
-        return (
-            <View style={styles.container}>
-                <Image
-                source={{uri: movie.posters.thumbnail}}
-                style={styles.thumbnail}
-                />
-                <View style={styles.rightContainer}>
-                    <Text style={styles.title}>{movie.title}</Text>
-                    <Text style={styles.year}>{movie.year}</Text>
-                </View>
+            <View>
+                <Text style={styles.title}> Raspberry Pi Status: </Text>
+                <Text> {this.state.isActive} </Text>
             </View>
         );
     }
 }
 
 var styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
-    rightContainer: {
-        flex: 1,
-    },
     title: {
-        fontSize: 20,
-        marginBottom: 8,
-        textAlign: 'center',
-    },
-    year: {
-        textAlign: 'center',
-    },
-    thumbnail: {
-        width: 53,
-        height: 81,
-    },
-    listView: {
-        paddingTop: 20,
-        backgroundColor: '#F5FCFF',
+        marginTop: 30,
     },
 });
 
